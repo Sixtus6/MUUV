14ed354f7caf578d48215e99b4a2cbf19ce06623
sixtus6 <xystuseton@gmail.com>
2023-10-04 16:48:06 +0100

    added googlemap helper

--- lib/screens/home/user/provider.dart
+++ lib/screens/home/user/provider.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart' as loc;
 
@@ -9,6 +10,18 @@ class UserGoogleMapProvider with ChangeNotifier {
   LatLng? picklocation;
   loc.Location location = loc.Location();
   String? address;
+  Position? userCurrentPosition;
+  var geolocation = Geolocator();
+  LocationPermission? _locationPermission;
+  List<LatLng> pLineCordinateList = [];
+  Set<Polyline> polylineset = {};
+  Set<Marker> markerSet = {};
+  Set<Circle> circleset = {};
+  String username = "";
+  String email = "";
+  bool openNavigationDraer = true;
+  bool activeNearbyDriverKeysLoaded = false;
+  BitmapDescriptor? activeNearbyIcon;
 
   Future<GoogleMapController> get controller => _controllerCompleter.future;
 
