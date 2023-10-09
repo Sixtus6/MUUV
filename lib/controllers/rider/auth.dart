90b6b606c52e183c7429f44c4590a78d16d70dd4
sixtus6 <xystuseton@gmail.com>
2023-10-09 02:18:01 +0100

    creating the rider model

---
+++ lib/controllers/rider/auth.dart
@@ -0,0 +1,150 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:muuv/model/user.dart';
+import 'package:muuv/screens/home/user/index.dart';
+import 'package:muuv/utils/helper.dart';
+import 'package:muuv/widget/arrow.dart';
+import 'package:nb_utils/nb_utils.dart';
+
+class RiderAuthProvider with ChangeNotifier {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  bool _isSignUpSuccessful = false;
+  bool get isSignUpSuccessful => _isSignUpSuccessful;
+  bool _isLoginSuccessful = false;
+  bool get isLoginSuccessful => _isLoginSuccessful;
+  UserModel? _user;
+  UserModel? get user => _user;
+
+  // Initialize the AuthProvider
+  RiderAuthProvider() {
+    _auth.authStateChanges().listen((firebaseUser) {
+      if (firebaseUser == null) {
+        _user = null;
+      } else {
+        _user = UserModel(
+            uid: firebaseUser.uid,
+            name: "",
+            emailAddress: "",
+            password: "",
+            phoneNumber: "",
+            address: "");
+      }
+      notifyListeners();
+    });
+  }
+  Future<void> loginWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+
+      // If login is successful, set the login success flag to true
+      _isLoginSuccessful = true;
+      notifyListeners();
+      toast("Login successfully");
+      await _fetchUserDetails(userCredential.user!.uid, email);
+      //saves user to shared preferences
+      saveUserToPrefs(_user!);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'INVALID_LOGIN_CREDENTIALS') {
+        _isLoginSuccessful = false;
+        notifyListeners();
+        toast("Email or password incorrect");
+      } else {
+        // toast("Something went wrong");
+        _isLoginSuccessful = false;
+        notifyListeners();
+      }
+
+      print('Error during login: ${e.message}');
+      _isLoginSuccessful = false;
+      notifyListeners();
+
+      rethrow;
+    } catch (e) {
+      print('Unexpected error during login: $e');
+      _isLoginSuccessful = false;
+      notifyListeners();
+      rethrow;
+    }
+  }
+
+  Future<void> signUpWithEmailAndPassword(String email, String password,
+      String name, String address, String phoneNumber) async {
+    try {
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      // If user creation is successful, we can now update additional user details
+      await _updateUserDetails(
+          userCredential.user!, name, phoneNumber, address);
+      _isSignUpSuccessful = true;
+      notifyListeners();
+      toast("Account created successfully");
+      await _fetchUserDetails(userCredential.user!.uid, email);
+      //saves user to shared preferences
+      saveUserToPrefs(_user!);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'email-already-in-use') {
+        _isSignUpSuccessful = false;
+        notifyListeners();
+
+        // Handle the case where email is already in use by an existing account
+        toast("Email already exist");
+      } else {
+        toast("Something went wrong");
+        _isSignUpSuccessful = false;
+        notifyListeners();
+
+        print('Error during sign up: ${e.message}');
+      }
+    } catch (e) {
+      toast("Something went wrong");
+      _isSignUpSuccessful = false;
+      notifyListeners();
+
+      print('Unexpected error during sign up: $e');
+      rethrow;
+    }
+  }
+
+  Future<void> _updateUserDetails(
+      User user, String name, String phoneNumber, String address) async {
+    try {
+      await FirebaseFirestore.instance.collection('users').doc(user.uid).set({
+        'fullName': name,
+        'phoneNumber': phoneNumber,
+        'address': address
+        // Add other details as needed
+      });
+    } catch (e) {
+      print('Error updating user details: $e');
+      rethrow;
+    }
+  }
+
+  Future<void> _fetchUserDetails(String uid, email) async {
+    // Fetch user details from Firestore using uid and update _userDetails
+    // Example: Fetching from a 'users' collection
+    DocumentSnapshot userSnapshot =
+        await FirebaseFirestore.instance.collection('users').doc(uid).get();
+    print(userSnapshot.data());
+    if (userSnapshot.exists) {
+      _user = UserModel(
+        uid: uid,
+        name: userSnapshot['fullName'],
+        emailAddress: email,
+        address: userSnapshot['address'],
+        phoneNumber: userSnapshot['phoneNumber'],
+        password: "",
+      );
+    }
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
---
+++ lib/model/rider.model.dart
@@ -0,0 +1,50 @@
+class RiderModel {
+  final String uid;
+  final String name;
+  final String emailAddress;
+  final String password;
+  final String phoneNumber;
+  final String address;
+  final String carModel;
+  final String carColor;
+  final String carPlateNumber;
+
+  RiderModel({
+    required this.carModel,
+    required this.carColor,
+    required this.carPlateNumber,
+    required this.uid,
+    required this.name,
+    required this.emailAddress,
+    required this.password,
+    required this.phoneNumber,
+    required this.address,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'uid': uid,
+      'name': name,
+      'emailAddress': emailAddress,
+      'password': password,
+      'phoneNumber': phoneNumber,
+      'address': address,
+      'carModel': carModel,
+      'carColor': carColor
+    };
+  }
+
+  factory RiderModel.fromJson(Map<String, dynamic> json) {
+    return RiderModel(
+      uid: json['uid'] as String,
+      name: json['name'] as String,
+      emailAddress: json['emailAddress'] as String,
+      phoneNumber: json['phoneNumber'] as String,
+      address: json['address'] as String,
+      carColor: '',
+      carModel: '',
+      carPlateNumber: '',
+      password: '',
+    );
+  }
+}
