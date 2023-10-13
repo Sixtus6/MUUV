592fb3a6760be10c7e6be3b59bec93b2096e7c1f
sixtus6 <xystuseton@gmail.com>
2023-10-13 13:48:10 +0100

    saved changes

--- lib/screens/home/rider/index.dart
+++ lib/screens/home/rider/index.dart
@@ -22,43 +22,48 @@ class _RiderHomeScreenState extends State<RiderHomePage> {
         onTap: () {
           FocusScope.of(context).unfocus();
         },
-        child: Stack(children: [
-          Consumer<RiderGoogleMapProvider>(builder: (context, provider, _) {
-            if (provider.controller != null) {
-              return GoogleMap(
-                onMapCreated: (controller) {
-                  provider.setController(controller);
-                  provider.setnewGoogleMapController(controller);
+        child: Consumer<RiderGoogleMapProvider>(
+          builder: (BuildContext context, provider, _) {
+            return Stack(children: [
+              provider.controller != null
+                  ? GoogleMap(
+                      onMapCreated: (controller) {
+                        provider.setController(controller);
+                        provider.setnewGoogleMapController(controller);
 
-                  provider.locateDriverPosition();
-                },
-                // onCameraMove: (CameraPosition? position) {
-                //   //    print("moving ${provider.pickLocation}");
-                //   if (provider.pickLocation != position!.target) {
-                //     provider.setpickLocation(position.target);
-                //   }
-                // },
-                // onCameraIdle: () {
-                //   provider.getAddressFromLatLng();
-                // },
-                initialCameraPosition: const CameraPosition(
-                  target: LatLng(37.7749, -122.4194),
-                  zoom: 11.0,
-                ),
-                mapType: MapType.normal,
-                myLocationButtonEnabled: true,
-                myLocationEnabled: true,
-                zoomGesturesEnabled: true,
-                zoomControlsEnabled: true,
-                // polylines: provider.polylineSet,
-                // markers: provider.markerSet,
-                // circles: provider.circleSet,
-              );
-            } else {
-              return Center(child: ShimmerLoader());
-            }
-          })
-        ]),
+                        provider.locateDriverPosition();
+                      },
+                      // onCameraMove: (CameraPosition? position) {
+                      //   //    print("moving ${provider.pickLocation}");
+                      //   if (provider.pickLocation != position!.target) {
+                      //     provider.setpickLocation(position.target);
+                      //   }
+                      // },
+                      // onCameraIdle: () {
+                      //   provider.getAddressFromLatLng();
+                      // },
+                      initialCameraPosition: const CameraPosition(
+                        target: LatLng(37.7749, -122.4194),
+                        zoom: 15,
+                      ),
+                      mapType: MapType.normal,
+                      myLocationButtonEnabled: true,
+                      myLocationEnabled: true,
+                      zoomGesturesEnabled: true,
+                      zoomControlsEnabled: true,
+                      // polylines: provider.polylineSet,
+                      // markers: provider.markerSet,
+                      // circles: provider.circleSet,
+                    )
+                  : Center(child: ShimmerLoader()),
+              Container(
+                color: Colors.black87.withOpacity(0.5),
+              )
+                  .withHeight(MediaQuery.of(context).size.height)
+                  .withWidth(double.infinity),
+            ]);
+          },
+        ),
       ),
     ));
   }
