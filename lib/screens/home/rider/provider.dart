e9d7db9f0e1bc6af68aee7a71ee4e150ec78b394
sixtus6 <xystuseton@gmail.com>
2023-10-13 07:42:23 +0100

    saved changes

---
+++ lib/screens/home/rider/provider.dart
@@ -0,0 +1,13 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class RiderGoogleMapProvider with ChangeNotifier {
+  Completer<GoogleMapController> _controllerCompleter = Completer();
+
+  final CameraPosition _kGooglePlex =
+      const CameraPosition(target: LatLng(37.7749, -122.4194), zoom: 11.0);
+
+  CameraPosition get kGooglePlex => _kGooglePlex;
+}
