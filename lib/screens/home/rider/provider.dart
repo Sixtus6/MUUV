03fd6dd728cf9a81d987db7fee146d1a7957534a
sixtus6 <xystuseton@gmail.com>
2023-10-13 07:45:12 +0100

    saved changes

--- lib/screens/home/rider/provider.dart
+++ lib/screens/home/rider/provider.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class RiderGoogleMapProvider with ChangeNotifier {
@@ -8,6 +9,17 @@ class RiderGoogleMapProvider with ChangeNotifier {
 
   final CameraPosition _kGooglePlex =
       const CameraPosition(target: LatLng(37.7749, -122.4194), zoom: 11.0);
-
   CameraPosition get kGooglePlex => _kGooglePlex;
+
+  late Geolocator _geolocator;
+  Geolocator get geolocator => _geolocator;
+
+  LocationPermission? _locationPermission;
+  LocationPermission? get locationPermission => _locationPermission;
+
+  String _status = "Offline";
+  String get status => _status;
+
+  bool _openNavigationDrawer = true;
+  bool get openNavigationDrawer => _openNavigationDrawer;
 }
