8ec17bd5858320af2db52e65e929b2072de1dce4
sixtus6 <xystuseton@gmail.com>
2023-10-15 22:52:23 +0100

    saved refactored chages

--- lib/widget/shimmer button.dart
+++ lib/widget/shimmer button.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:muuv/config/size.dart';
+import 'package:nb_utils/nb_utils.dart';
 import 'package:shimmer/shimmer.dart';
 
 class ShimmerActionButton extends StatelessWidget {
@@ -15,8 +17,25 @@ class ShimmerActionButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return isLoading
-        ? Container()
+        ? Container(
+            padding:
+                EdgeInsetsDirectional.all(SizeConfigs.getPercentageWidth(4)),
+            decoration: boxDecorationRoundedWithShadow(8,
+                backgroundColor: Colors.green),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.red)),
+              onPressed: onPressed,
+              child: Text(
+                "Accept",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          )
         : ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.red)),
             onPressed: onPressed,
             child: Text(
               label,
@@ -46,3 +65,42 @@ class ShimmerActionButton extends StatelessWidget {
 //               ),
 //             ),
 //           )
+
+class AnimatedButton extends StatefulWidget {
+  @override
+  _AnimatedButtonState createState() => _AnimatedButtonState();
+}
+
+class _AnimatedButtonState extends State<AnimatedButton> {
+  bool _isPressed = false;
+
+  void _togglePress() {
+    setState(() {
+      _isPressed = !_isPressed;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (_) {
+        _togglePress();
+      },
+      onTapUp: (_) {
+        _togglePress();
+      },
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 200),
+        decoration: BoxDecoration(
+          color: _isPressed ? Colors.blue.withOpacity(0.5) : Colors.blue,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        padding: const EdgeInsets.all(16.0),
+        child: Text(
+          'Press Me',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
