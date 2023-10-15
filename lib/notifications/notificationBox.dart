0ffcd208c7def2c1c3ab05b174f82bfe49f25da6
sixtus6 <xystuseton@gmail.com>
2023-10-15 22:12:20 +0100

    saved refactored chages

--- lib/notifications/notificationBox.dart
+++ lib/notifications/notificationBox.dart
@@ -80,7 +80,14 @@ class _NotificationDialogBoxState extends State<NotificationDialogBox> {
                   thickness: 2,
                   color: ColorConfig.primary,
                 ),
-                ShimmerEffect(),
+                ShimmerActionButton(
+                  isLoading: true, // Set to true to display shimmer effect
+                  onPressed: () {
+                    // Handle button press
+                    print('Button pressed!');
+                  },
+                  label: 'Press me',
+                ),
               ],
             ),
           ),
