ac06a2dfdb69f4a307753985189d39bd37920f6e
sixtus6 <xystuseton@gmail.com>
2023-10-16 02:07:59 +0100

    added driver new screen

--- lib/screens/trip/rider/provider.dart
+++ lib/screens/trip/rider/provider.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class RiderTripGoogleMapProvider with ChangeNotifier {
-    Completer<GoogleMapController> _controllerCompleter = Completer();
-    
+  Completer<GoogleMapController> _controllerCompleter = Completer();
+
+  final CameraPosition _kGooglePlex =
+      const CameraPosition(target: LatLng(37.7749, -122.4194), zoom: 11.0);
+  CameraPosition get kGooglePlex => _kGooglePlex;
 }
