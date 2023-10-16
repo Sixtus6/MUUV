aad342116db572724a48848ae75f8281d344b37e
sixtus6 <xystuseton@gmail.com>
2023-10-16 02:23:36 +0100

    added driver new screen

--- lib/screens/trip/rider/provider.dart
+++ lib/screens/trip/rider/provider.dart
@@ -2,9 +2,12 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class RiderTripGoogleMapProvider with ChangeNotifier {
+
+  
   Completer<GoogleMapController> _controllerCompleter = Completer();
 
   final CameraPosition _kGooglePlex =
@@ -25,4 +28,10 @@ class RiderTripGoogleMapProvider with ChangeNotifier {
 
   PolylinePoints _polylinePoints = PolylinePoints();
     PolylinePoints get polylinePoints => _polylinePoints;
+
+  late Geolocator _geolocator;
+  Geolocator get geolocator => _geolocator;
+
+     BitmapDescriptor? _iconAnimatedMarker;
+  BitmapDescriptor? get iconAnimatedMarker => _iconAnimatedMarker;
 }
